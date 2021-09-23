a9b2003f37882ee1255d7bb4ce6c7bcc1456b982
Miguel Oliveira <miguel.nicolau.oliveira762@gmail.com>
2021-09-23 21:32:35 +0100

    Add AspectJ example

---
+++ src/main/java/miguel/oliveira/demo/aop/RecordEvent.java
@@ -0,0 +1,12 @@
+package miguel.oliveira.demo.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RecordEvent {
+  // Used to mark methods for event registering
+}
